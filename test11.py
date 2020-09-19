ea27526f83638cc39f014008a4219569a48fa578
ATHARVA MAHINDRAKAR <53110598+atharva-9@users.noreply.github.com>
2020-09-19 15:56:50 +0530

    Update test11.py

--- test11.py
+++ test11.py
@@ -1,6 +1,6 @@
 from hcsr04 import HCSR04
 
-sensor = HCSR04(trigger_pin=16, echo_pin=17, echo_timeout_us=10000)
+sensor = HCSR04(trigger_pin=3, echo_pin=2, echo_timeout_us=10000)
 
 try:
     distance = sensor.distance_cm()
